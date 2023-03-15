8a0c79e7f12526cf5fccb615e1bec7d539c5c06a
ritesh kumar <Ritesh74461@gmail.com>
2023-03-15 18:26:16 +0530

    new

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
-        obj.set("ritesh");
-        System.out.print(obj.get());
+
+        obj.setName("ritesh");
+
+        System.out.print(obj.getName());
     }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,10 +2,10 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    public void set(String name){
+    public void setName(String name){
         this.name = name;
     }
-    public String get(){
+    public String getName(){
         return name;
     }
 }
